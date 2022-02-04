9b84dfe209f7fabef81b2f0c7ff1058fb83b770c
Neil Ghosh <neil.ghosh@gmail.com>
2022-02-04 20:04:38 +0000

    formatted

--- function-generator.js
+++ function-generator.js
@@ -1,22 +1,22 @@
 function* myFunction(index) {
     while (index < 5) {
-      yield index + "-value";
-      index++;
+        yield index + "-value";
+        index++;
     }
-  }
-  
-  const iterator = myFunction(0);
+}
+
+const iterator = myFunction(0);
 
 //   for (var v; v = iterator.next().value; ) {      //will return when undefined is yield 
 //     console.log(v);
 //     // Coud laso return when done.
 //   }
 
-  while(true) {
-      nextItem = iterator.next();
-      if(nextItem.done) {
-          return;
-      } else {
-          console.log(nextItem.value);
-      }
-  }
+while (true) {
+    nextItem = iterator.next();
+    if (nextItem.done) {
+        return;
+    } else {
+        console.log(nextItem.value);
+    }
+}
